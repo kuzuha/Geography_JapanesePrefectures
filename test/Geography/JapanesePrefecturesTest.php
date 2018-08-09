bae3901f5d0366e194d718bf012347e97f955e12
Ozawa Masayuki <ozawa@coroq.com>
2018-08-09 15:28:18 +0900

    Use PSR-4 autoloading
    The class name was changed to \Geography\JapanesePrefectures.

--- test/Geography/JapanesePrefecturesTest.php
+++ test/Geography/JapanesePrefecturesTest.php
@@ -1,9 +1,7 @@
 <?php
+use \Geography\JapanesePrefectures;
 
-require_once dirname(__FILE__) . '/../../src/Geography/JapanesePrefectures.php';
-require_once 'PHPUnit/Framework/Assert/Functions.php';
-
-class Geography_JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
+class JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @test
@@ -11,7 +9,7 @@ class Geography_JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
      */
     function prefectures()
     {
-        assertSame(47, count(Geography_JapanesePrefectures::prefectures()));
+        $this->assertSame(47, count(JapanesePrefectures::prefectures()));
     }
 
     /**
@@ -20,7 +18,7 @@ class Geography_JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
      */
     function regions()
     {
-        assertSame(11, count(Geography_JapanesePrefectures::regions()));
+        $this->assertSame(11, count(JapanesePrefectures::regions()));
     }
 
     /**
@@ -30,7 +28,7 @@ class Geography_JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
     function prefecturesIn()
     {
         $expected = array('茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '山梨県');
-        assertSame($expected, Geography_JapanesePrefectures::prefecturesIn('関東'));
+        $this->assertSame($expected, JapanesePrefectures::prefecturesIn('関東'));
     }
 
     /**
@@ -39,7 +37,7 @@ class Geography_JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
      */
     function prefecturesId()
     {
-        assertSame(30, Geography_JapanesePrefectures::prefecturesId('和歌山県'));
+        $this->assertSame(30, JapanesePrefectures::prefecturesId('和歌山県'));
     }
 
     /**
@@ -48,10 +46,10 @@ class Geography_JapanesePrefecturesTest extends PHPUnit_Framework_TestCase
      */
     function prefecturesInfos()
     {
-        $info_list = Geography_JapanesePrefectures::prefecturesInfos();
-        assertSame(47, count($info_list));
-        assertSame(array('id', 'name', 'region', 'roman'), array_keys($info_list[0]));
-        assertSame('北海道', $info_list[0]['name']);
-        assertSame('Hokkaido', $info_list[0]['roman']);
+        $info_list = JapanesePrefectures::prefecturesInfos();
+        $this->assertSame(47, count($info_list));
+        $this->assertSame(array('id', 'name', 'region', 'roman'), array_keys($info_list[0]));
+        $this->assertSame('北海道', $info_list[0]['name']);
+        $this->assertSame('Hokkaido', $info_list[0]['roman']);
     }
 }
